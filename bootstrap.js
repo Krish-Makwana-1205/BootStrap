f3d41e8a4805e7e6e693cb4dfe2745ed51cbd901
Krish-Makwana-1205 <krish.rajput1205@gmail.com>
2024-06-12 13:26:21 +0530

    Error full update

--- bootstrap.js
+++ bootstrap.js
@@ -1,75 +1,76 @@
-// let iid = document.getElementById("ID");
-// let iname = document.getElementById("Name");
-// let irate = document.getElementById("Rate");
-let arr = [];
-let count = 1;
-let obj = {
-    docid : document.getElementById("row1")
-};
-arr.push(obj);
-function change(event) {
-    let iid = document.getElementById("ID");
-    let iname = document.getElementById("Name");
-    let irate = document.getElementById("Rate");
-    for(let i = 0; i < count; ++i){
-        if(event.target.parentElement.parentElement == arr[i].docid){
-            console.log(arr[i].docid.childNodes[0]);
-            arr[i].docid.childNodes[0].innerHTML = iid.value;
-            arr[i].docid.childNodes[1].innerHTML = iname.value;
-            arr[i].docid.childNodes[2].innerHTML = irate.value;
-            break;
+//localStorage['0'] = '0';
+function render(){
+    //localStorage.clear();
+    let x;
+    for(let i = 1; i <= localStorage.length; ++i){
+        x = JSON.parse(localStorage.getItem(i+1));
+        if(x != null){
+            make(x); 
         }
     }
+    //localStorage.setItem('0','0');
 }
-function add(x) {
-    let iid = document.getElementById("ID");
-    let iname = document.getElementById("Name");
-    let irate = document.getElementById("Rate");
-    ++count;
-    let dets = {};
-    Newr = document.createElement("div");
-    dets.docid=Newr;
-    arr.push(dets);
-    Newr.setAttribute("class", "row");
-    Newr.setAttribute("id", "row");
-    let col1 = document.createElement("div");
-    let col2 = document.createElement("div");
-    let col3 = document.createElement("div");
-    let col4 = document.createElement("div");
-    let col5 = document.createElement("div");
-    let editb = document.createElement("button");
-    let deleteb = document.createElement("button");
-    col1.setAttribute("class", "col");
-    col1.innerHTML = iid.value;
-    col2.setAttribute("class", "col");
-    col2.innerHTML = iname.value;
-    col3.setAttribute("class", "col");
-    col3.innerHTML = irate.value;
-    col4.setAttribute("class", "col");
-    editb.addEventListener("click", change);
-    console.log(count);
-    editb.setAttribute("class", "btn btn-info");
-    editb.innerHTML = "Edit";
+function make(x){
+    console.log("loj");
+    console.log(x);
+    console.log("loj");
+    let newr = document.createElement('div');
+    newr.setAttribute('class','row');
+    let col1 = document.createElement('div');
+    col1.setAttribute('class','col');
+    col1.innerHTML = x.id;
+    let col2 = document.createElement('div');
+    col2.setAttribute('class','col');
+    col2.innerHTML = x.name;
+    let col3 = document.createElement('div');
+    col3.setAttribute('class','col');
+    col3.innerHTML = x.rate;
+    let col4 = document.createElement('div');
+    col4.setAttribute('class','col');
+    let editb = document.createElement('button');
+    editb.innerHTML = 'Edit';
+    editb.setAttribute('class','btn btn-info');
     col4.appendChild(editb);
-    col5.setAttribute("class", "col");
-    deleteb.addEventListener("click", Delete);
-    deleteb.setAttribute("class", "btn btn-danger");
-    deleteb.innerHTML = "Delete";
+    let col5 = document.createElement('div');
+    col5.setAttribute('class','col');
+    let deleteb = document.createElement('button');
+    deleteb.innerHTML = 'Delete';
+    deleteb.setAttribute('class','btn btn-danger');
+    deleteb.setAttribute('onclick', 'Delete(event)')
     col5.appendChild(deleteb);
-    Newr.appendChild(col1);
-    Newr.appendChild(col2);
-    Newr.appendChild(col3);
-    Newr.appendChild(col4);
-    Newr.appendChild(col5);
-    document.body.appendChild(Newr)
+    newr.appendChild(col1);
+    newr.appendChild(col2);
+    newr.appendChild(col3);
+    newr.appendChild(col4);
+    newr.appendChild(col5);
+    let ref = document.getElementById('ref');
+    document.body.insertBefore(newr, ref);
+}
+function add(){
+    let id = document.getElementById("ID");
+    let name = document.getElementById("name");
+    let rate = document.getElementById("rate");
+    //console.log(name);
+    console.log(localStorage.length);
+    let obj = {
+        id : id.value,
+        name : name.value,
+        rate : rate.value
+    }
+    let ins = JSON.stringify(obj);
+    console.log(ins);
+    localStorage.setItem(localStorage.length+1, ins);
+    console.log(localStorage.length);
+    console.log(localStorage);
+    window.location.replace("bootstrap.html");
 }
 function Delete(event) {
-    event.target.parentElement.parentElement.remove();
-    for(let i = 0; i < count; ++i){
-        if(event.target.parentElement.parentElement == arr[i].docid){
-            arr.splice(count, count);
-            break;
+    //localStorage.clear();
+    for(let i = 1; i <= localStorage.length-1; ++i){
+        x = JSON.parse(localStorage.getItem(i+1));
+        if(x.id == event.target.parentElement.parentElement.childNodes[0].innerHTML){
+            localStorage.removeItem(i+1);
         }
     }
-    --count;
+    event.target.parentElement.parentElement.remove();
 }
